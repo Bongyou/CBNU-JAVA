6051d491a3bb9003a022d7fc18f975b33044dc3e
qhddb <qhddb@192.168.1.12>
2017-10-20 16:27:04 +0900

    171020 실습

---
+++ 171020/src/Manager.java
@@ -0,0 +1,15 @@
+
+public class Manager extends Employee{
+	private String position;
+	
+	public Manager(String empNo,String name,String part,String position) {
+		super(empNo,name,part);
+		this.position = position;
+	}
+	
+	public String addStr() {
+		String result = super.resultStr();
+		result += "Á÷Ã¥ : "+position+"\n";
+		return result;
+	}
+}
